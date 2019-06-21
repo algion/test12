1438f4a646a04fd685901bc2f9885141137c2a1f
Andrey <mralgion@gmail.com>
2019-06-21 13:38:26 +0300

    add first

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 let $searchForm = $('#search-form');
-
+let $bookList = $ ('#book-list');
+let arrBooks = []
 $searchForm.on("submit", function (event) {
   event.preventDefault();
   let query = $(this).find('[name="srch-term"]').val().replace(/\s/g, "+");
@@ -17,7 +18,14 @@ function getBooks(query) {
     method: "GET",
     data: `q=${query}`
   }).done(function (response) {
-    console.log(response);
+    arrBooks = response.items;
+    $bookList.empty();
+    arrBooks.forEach(function (book){
+      $('<a href="">').addClass('list-group-item')
+      .text(book.volumeInfo.title)
+      .attr('data-id', book.id)
+      .appendTo($bookList);
+    })
   }).fail(function (error) {
     console.log(error);
   });
